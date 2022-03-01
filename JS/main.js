76fa7cc41c33d86e55f196d4e17be0f97f18c5cc
Ismaeel <99956213+LeafVI@users.noreply.github.com>
2022-03-01 20:28:39 +0500

    Website Finished

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,19 @@
+const responsive = {
+  0 : {
+    items: 1
+  },
+  320 : {
+    items: 1
+  },
+  560 : {
+    items: 2
+  },
+  960 : {
+    items: 3
+  },
+
+}
+
 $(document).ready(function(){
    
   $navbar = $('.dropdown');
@@ -7,5 +23,23 @@ $(document).ready(function(){
   $togglecollapse.click(function(){
     $navbar.toggleClass('collapse');
   })
+ 
+  // owl carousel for articles slide
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
+  });
 
+
+  // click to scroll to top
+  $('.moveup span').click(function(){
+    $('html, body').animate({
+      scrollTop:0
+    }, 1000)
+  })
 });
